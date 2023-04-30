1e5ffdb976a11f9ec58cc8cc05eda021c18ed587
Sam Phillips <samdphillips@gmail.com>
2023-04-29 20:34:27 -0700

    Nest the read syntax to adjust for splicing with module-begin

--- main.rkt
+++ main.rkt
@@ -3,6 +3,8 @@
 (module reader syntax/module-reader
   #:language    read
   #:read        read-c-expr
-  #:read-syntax read-c-expr-syntax
+  #:read-syntax (lambda (src in)
+                  (with-syntax ([body (read-c-expr-syntax src in)])
+                    #'(body)))
   #:whole-body-readers? #t
   (require c-expr/read))
