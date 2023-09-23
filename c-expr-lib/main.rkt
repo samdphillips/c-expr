57b065457243f94fd29e6fc5db9aa592cddc2450
Sam Phillips <samdphillips@gmail.com>
2023-09-22 17:29:42 -0700

    Make `#lang c-expr` work

--- c-expr-lib/main.rkt
+++ c-expr-lib/main.rkt
@@ -1,1 +1,8 @@
 #lang racket/base
+
+(module reader syntax/module-reader
+  #:language    read
+  #:read        read-c-expr
+  #:read-syntax read-c-expr-syntax
+  #:whole-body-readers? #t
+  (require c-expr/read))
