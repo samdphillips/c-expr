50c7b98a5ea73271551fde56fbbc7fb7557b8037
Sam Phillips <samdphillips@gmail.com>
2023-09-22 17:29:42 -0700

    Make example more package like

---
+++ c-expr-example/info.rkt
@@ -0,0 +1,9 @@
+#lang info
+
+(define name "c-expr-example")
+(define collection "c-expr")
+(define version "0.99")
+(define deps '("base" "c-expr-lib"))
+(define build-deps '())
+(define pkg-authors '(samdphillips@gmail.com))
+(define license 'Apache-2.0)
